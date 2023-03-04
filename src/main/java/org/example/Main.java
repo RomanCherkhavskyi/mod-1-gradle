1140951fd106fc8419832c14fd5fad422b0c7c2d
Roman Cherkhavskyi <romko-chera@ukr.net>
2023-03-04 11:30:38 +0200

    dev mod 1 with gradle
    Base commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        person.setName("Roman");
+        person.setLastName("Cherkhavskyi");
+
+
+
+        System.out.println(person.toString());
+    }
+}
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Person {
+    private String name;
+
+    private String lastName;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
